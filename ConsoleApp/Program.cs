d3295ec2bf50c42a9ae374f60590464050d0668f
Zack <72839443+FoxieZack@users.noreply.github.com>
2022-08-16 07:10:57 +0000

    8 / 15 12 (16th now)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,100 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Linq;
-
+using System.Collections;
 
 namespace Program{
-    public class MainCLass{
+    public class MainClass{
+        
+        static Pokemon Charmander = new Pokemon(exp: 0, level: 1, health: 69, name: "Charmander");
+        static Pokemon Squirtle = new Pokemon(exp: 0, level: 1, health: 65,name: "Squirtle", move1_name: "Water gun", move2_name: "Rush", move3_name: "scratch", move1_attack: 19, move2_attack: 17, move3_attack: 14);
+        
+        
+        
+
+        
         
         
         public static void Main(){
-
-            //var Pokadex = CurrentParty.ToDictionary(kv => kv.Key, kv => kv.Value);
-
+            
+            
+            
             
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("would you like too pick a starter pokemon? you have 3 choicse");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($" {Pokadex["Charmander"]}? (1) \n");
+            Console.WriteLine($" Your first choise is {MainClass.Charmander.Name}  (1) \n");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("open your Bag? (2) \n");
+            Console.WriteLine("  (2) \n");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Flee? (3) \n");
+            Console.WriteLine(" (3) \n");
             Console.ForegroundColor = ConsoleColor.White;
 
             ConsoleKey Starter = Console.ReadKey(true).Key;
 
+           
+
             if(Starter == ConsoleKey.D1){
-                Console.WriteLine($"you choose {Pokadex["Charmander"]}!!");
+                Console.WriteLine("you choose !!");
 
-                CurrentParty.Add("Charmander", Pokadex["Charmander"]);
-                //Pokadex.Remove("key");
                 
-
-
-                foreach(var Name in Pokadex){
-                    Console.WriteLine(Name.Key + Name.Value.Exp + Name.Value.lvl);
-                }
-
-                foreach(var Name in CurrentParty){
-                    Console.WriteLine(Name.Key + Name.Value.Exp + Name.Value.lvl);
-                }
+                
+                
+                
+                
             }
-            
-            
-            
-
+         
         }
+
         
-        public static Dictionary<string, (int lvl, int Exp)> Pokadex = new Dictionary<string, (int lvl, int Exp)>{
-            { "Charmander", (0, 0)},
-            { "Squirtle", (0, 0)},
-            { "Bulbasaur", (0, 0)},
-            { "Pikachu", (0, 0)}
-        };
 
         //public static Dictionary<string, (int lvl, int Exp)> PlayerPokadex = new Dictionary<string, (int lvl, int Exp)>{};
 
-        public static Dictionary<string, (int lvl, int Exp)> CurrentParty = new Dictionary<string, (int lvl, int Exp)>{};
+        //public static Dictionary<string, (int lvl, int Exp)> CurrentParty = new Dictionary<string, (int lvl, int Exp)>();
         
-        public static Dictionary<string, (int RestoreAmount, int Amount)> Bag = new Dictionary<string, (int RestoreAmount, int Amount)>{};
+        //public static Dictionary<string, (int RestoreAmount, int Amount)> Bag = new Dictionary<string, (int RestoreAmount, int Amount)>();
 
-        public static Dictionary<string, (int RestoreAmount, int Amount)> Shop = new Dictionary<string, (int RestoreAmount, int Amount)>{};
+        //public static Dictionary<string, (int RestoreAmount, int Amount)> Shop = new Dictionary<string, (int RestoreAmount, int Amount)>();
 
-
-
-       /* public static Dictionary<string, (int price, int Amount)>  gints = new Dictionary<string, (int Price, int Amount)>
-        {
-            {"Attack Points: ", (10, 0)},
-            {"Defense Points: ", (10, 0)},
-            {"Super Points: ", (10, 0)},
-            {"N / A", (10, 0)}
-        };
-        */
         
     } 
 
     class Pokemon{
-        public string Name;
-        public int health;
-        public int level;
-        public float Exp = 0f;
+
+
+        public int Health { get; set; }
+        public float Exp { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public Pokemon(float exp, int level, int health, string name, string move1_name,string move2_name, string move3_name, int move1_attack, int move2_attack, int move3_attack)
+        {
+            this.Health = health;
+            this.Exp = exp;
+            this.Level = level;
+            this.Name = name;
+
+            this.Move1_Name = move1_name;
+            this.Move2_Name = move2_name;
+            this.Move3_Name = move3_name;
+
+            this.Move1_Attack = move1_attack;
+            this.Move2_Attack = move2_attack;
+            this.Move3_Attack = move3_attack;
+            
+        }
+
+        public Pokemon(){
+            
+        }
+        
         ////////////////////////////////////////
-        public string Move1_Name;
-        public string Move2_Name;
-        public string Move3_Name;
+        public string Move1_Name { get; set; }
+        public string Move2_Name { get; set; }
+        public string Move3_Name { get; set; }
         ////////////////////////////////////////
-        public int Move1_attack;
-        public int Move2_attack;
-        public int Move3_attack;
+        public int Move1_Attack { get; set; }
+        public int Move2_Attack { get; set; }
+        public int Move3_Attack { get; set; }
 
         public static bool PlayerAttack = false;
         public static bool PlayerBag = false;
         public static bool PlayerFlee = false;
 
-        public static void TallGrass(){
+        public void TallGrass(){
             Random Rnd = new Random();
             int FindPokemon = Rnd.Next(1, 5);
 
@@ -148,7 +154,29 @@ namespace Program{
                 
             }
         }
+
+        private List<Pokemon> internalList = new List<Pokemon>();
+        public class PokemonParty : IReadOnlyList<Pokemon>{
+            
+        }
     }
+
+    
+        
+        
+        // TODO: Implement the IReadOnlyList interface to let other classes read from your internal list. VS can auto-do this for you. 
+        // Why not implement IList directly? You may want to constrain the way pokemon are added to the party in a way that is not expected for a regular list interface. 
+
+        /*Do you see those angle brackets? < and > ? Those are indicating generic type arguments.
+        But Generics are a whole big branch and you don't have to start with that just now.
+        This lets you have a list of pokemon
+        And is a way that you can reference the type of other classes.
+        You could do it differently, since there's only up to 6 pokemon in a party.
+        */
+    
+    
+
+          
 }
 
 
