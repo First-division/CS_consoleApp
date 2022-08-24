fa9bd0202201634e8617b2ede91b0b00732b8265
Zack <72839443+FoxieZack@users.noreply.github.com>
2022-08-24 17:06:14 +0000

    8 / 24

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,15 +5,15 @@ using System.Collections;
 namespace Program {
     public class MainClass {
         
-        static Pokemon Charmander = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Charmander", Idnumber: 004, move1_name: "Ember", move2_name: "Scratch", move3_name: "Rush", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Squirtle = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Squirtle", Idnumber: 007,move1_name: "Water gun", move2_name: "Rush", move3_name: "scratch", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon blubasaur = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Blubasaur", Idnumber: 001, move1_name: "Vine wip", move2_name: "Seed bomb", move3_name: "scratch", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Pigy = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Pigy", Idnumber: 000, move1_name: "Wind slash", move2_name: "Tackle", move3_name: "kick", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Eevee = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Eevee", Idnumber: 133, move1_name: "tackle", move2_name: "Tail Wip", move3_name: "Bite", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Cterpie = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Capterpie", Idnumber: 010, move1_name: "Bug Bite", move2_name: "String Shoot", move3_name: "Nap", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Geodude = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Geodude", Idnumber: 074, move1_name: "Rockc Punch", move2_name: "Light Punch", move3_name: "", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Houndour = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Houndour", Idnumber: 228, move1_name: "Dark Flare", move2_name: "Dark Bite", move3_name: "howel", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
-        static Pokemon Torrcat = new Pokemon(exp: 0, level: 1, health: Pokemon.RndHealth(), name: "Torrcat", Idnumber: 726, move1_name: "Fire Fang", move2_name: "Claw Slash", move3_name: "FlameThrower", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Charmander = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Charmander", Idnumber: 004, move1_name: "Ember", move2_name: "Scratch", move3_name: "Rush", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Squirtle = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Squirtle", Idnumber: 007,move1_name: "Water gun", move2_name: "Rush", move3_name: "scratch", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon blubasaur = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Blubasaur", Idnumber: 001, move1_name: "Vine wip", move2_name: "Seed bomb", move3_name: "scratch", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Pigy = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Pigy", Idnumber: 000, move1_name: "Wind slash", move2_name: "Tackle", move3_name: "kick", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Eevee = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Eevee", Idnumber: 133, move1_name: "tackle", move2_name: "Tail Wip", move3_name: "Bite", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Cterpie = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Capterpie", Idnumber: 010, move1_name: "Bug Bite", move2_name: "String Shoot", move3_name: "Nap", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Geodude = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Geodude", Idnumber: 074, move1_name: "Rockc Punch", move2_name: "Light Punch", move3_name: "", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Houndour = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(), Ohealth: Pokemon.RndHealth(), name: "Houndour", Idnumber: 228, move1_name: "Dark Flare", move2_name: "Dark Bite", move3_name: "howel", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
+        static Pokemon Torrcat = new Pokemon(exp: 0, level: 1, maxhealth: Pokemon.RndHealth(),Ohealth: Pokemon.RndHealth(), name: "Torrcat", Idnumber: 726, move1_name: "Fire Fang", move2_name: "Claw Slash", move3_name: "FlameThrower", move1_attack: Pokemon.RndAttack(), move2_attack: Pokemon.RndAttack(), move3_attack: Pokemon.RndAttack());
         
         
         public static Bag SmllHeal = new Bag(name: "Small Heal", amount: 1, decript: "Heals 20 Health of any 1 pokemon per item");
@@ -168,7 +168,7 @@ namespace Program {
                 case 1:
                 Console.WriteLine($"Your now Fighting {MainClass.Pigy.Name} \n and it level {MainClass.Pigy.Level}");
 
-                while (Caught = true || MainClass.Pigy.Health <= 0 || Pokemon.PokemonParty.internalList[0].Health <= 0 || Pokemon.PokemonParty.internalList[1].Health <= 0 || Pokemon.PokemonParty.internalList[2].Health <= 0) {
+                while (Caught = true || MainClass.Pigy.oHealth <= 0 || Pokemon.PokemonParty.internalList[0].oHealth <= 0 || Pokemon.PokemonParty.internalList[1].oHealth <= 0 || Pokemon.PokemonParty.internalList[2].oHealth <= 0) {
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("Would you like too.... press the number to sleact your option \n");
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -200,7 +200,25 @@ namespace Program {
                         case ConsoleKey.D2 or ConsoleKey.NumPad2:
                         
                         // make a new class full of itams and healing props with a list called "Bag"
-                        Console.WriteLine("what do you want to do in your bag? you have {bag let them heal pokemon and or do nothing}");
+                        Console.WriteLine($"what do you want to do in your bag? you have ");
+                        foreach (var item in Bag.PokemonBag.Pokebag) {
+                            Console.WriteLine(item);
+                            Thread.Sleep(250);
+                        }
+                        Console.WriteLine("would you like to use a item on your pokemon too heal them? (1) or do nothing (2)");
+                        var PlayerBag = Console.ReadKey(true).Key;
+                        switch (PlayerBag) {
+                            case ConsoleKey.D1 or ConsoleKey.NumPad1:
+                            foreach (var item in Pokemon.PokemonParty.internalList) {
+                                Console.WriteLine(item);
+                                Thread.Sleep(250);
+                            }
+                            if (Pokemon.PokemonParty.internalList[0].oHealth > Pokemon.PokemonParty.internalList[0].MaxHealth){
+                                // you can heal your first pokemon
+                            }
+                            break;
+                            
+                        }
                         break;
 
                         case ConsoleKey.D3 or ConsoleKey.NumPad3: // catch poekmon
@@ -261,13 +279,15 @@ namespace Program {
     }
 
     class Pokemon {
-        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int oHealth { get; set; }
         public float Exp { get; set; }
         public int Level { get; set; }
         public string Name { get; set; }
-        public Pokemon(float exp, int level, int health, string name, int Idnumber, string move1_name,string move2_name, string move3_name, int move1_attack, int move2_attack, int move3_attack)
+        public Pokemon(float exp, int level, int maxhealth, int Ohealth, string name, int Idnumber, string move1_name,string move2_name, string move3_name, int move1_attack, int move2_attack, int move3_attack)
         {
-            this.Health = health;
+            this.MaxHealth = maxhealth;
+            this.oHealth = Ohealth;
             this.Exp = exp;
             this.Level = level;
             this.Name = name;
@@ -300,7 +320,8 @@ namespace Program {
         public static bool PlayerFlee = false;
 
         public static int FindPokemon() => Random.Shared.Next(1, 5);
-        public static int RndHealth() => Random.Shared.Next(39, 69);
+        public static int RndHealth() =>  rnd.Next(39, 69);
+        static Random rnd = new Random();
         public static int RndAttack() => Random.Shared.Next(13, 41);
 
        
@@ -316,7 +337,7 @@ namespace Program {
         }
 
             public override string ToString() {
-                return "Party: " + Name + ": Health: " + Health + " Lvl: " + Level;
+                return "Party: " + Name + ": Health: " + MaxHealth + " / " + oHealth + " Lvl: " + Level;
             }
 
         
